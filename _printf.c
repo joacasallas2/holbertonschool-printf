d2f8c0df3d1f5c311ce2dc69e5ff91bb948fa4c6
joacasallas2 <proyecta.ds@gmail.com>
2023-11-27 13:44:14 -0500

    delete main.c

--- _printf.c
+++ _printf.c
@@ -2,9 +2,16 @@
 /**
  * print - Function that prints everything
  * @format: This is the string that contains the text to be written to stdout
- * Return: Nothing (void function)
+ * Return: the number of characters printed (excluding the null byte used to
+ * end output to strings)
  */
 int _printf(const char *format, ...)
 {
-       
+	va_list args;
+	va_start(args, format);
+	void *buffer;
+	buffer = malloc(BUFFSIZE);
+	
+	write(1, buffer, strlen(buffer));
+
 }
